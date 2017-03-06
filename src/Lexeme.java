b312adf381f84370bc46bf7fea87c4b66cf5bda5
Abdo-Sameh <abdo.sameh2000@gmail.com>
2017-03-06 16:58:16 +0200

    initial functions

--- src/Lexeme.java
+++ src/Lexeme.java
@@ -1,6 +1,18 @@
 
 public class Lexeme {
-	public int index = -1;
-	public String value = "";
-	public String token = "";
+	public int index;
+	public String value;
+	public String token;
+	
+	public Lexeme() {
+		this.index = -1;
+		this.value = "";
+		this.token = "";
+	}
+
+	public Lexeme(int index, String value, String token) {
+		this.index = index;
+		this.value = value;
+		this.token = token;
+	}
 }
